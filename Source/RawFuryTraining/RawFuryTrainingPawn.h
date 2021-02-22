2f17f69b200f9e7d6960dd7648d0191653281608
Alexandru Oprea <oalexprea@yahoo.com>
2021-02-22 17:47:06 +0200

    cleanup the pawn class in cpp

--- Source/RawFuryTraining/RawFuryTrainingPawn.h
+++ Source/RawFuryTraining/RawFuryTrainingPawn.h
@@ -10,109 +10,13 @@ UCLASS(Blueprintable)
 class ARawFuryTrainingPawn : public APawn
 {
 	GENERATED_BODY()
-
-	/* The mesh component */
-	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class UStaticMeshComponent* ShipMeshComponent;
-
-	/** The camera */
-	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* CameraComponent;
-
-	/** Camera boom positioning the camera above the character */
-	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
 public:
 	ARawFuryTrainingPawn();
 
-	/** Offset from the ships location to spawn projectiles */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite )
-	FVector GunOffset;
-	
-	/* How fast the weapon will fire */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
-	float FireRate;
+// Components
+protected:
+    UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    class UStaticMeshComponent* ShipMeshComponent;
 
-	/* The speed our ship moves around the level */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
-	float MoveSpeed;
-
-	/** Sound to play each time we fire */
-	UPROPERTY(Category = Audio, EditAnywhere, BlueprintReadWrite)
-	class USoundBase* FireSound;
-
-	// Begin Actor Interface
-	virtual void Tick(float DeltaSeconds) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
-	// End Actor Interface
-
-	/* Fire a shot in the specified direction */
-	void FireShot(FVector FireDirection);
-
-	/* Handler for the fire timer expiry */
-	void ShotTimerExpired();
-
-
-	UPROPERTY(EditAnywhere)
-	int32 MyInt1;
-
-    UPROPERTY(EditDefaultsOnly)
-	int32 MyInt2;
-
-    UPROPERTY(EditInstanceOnly)
-    int32 MyInt3;
-
-    UPROPERTY(VisibleAnywhere)
-    float MyInt4;
-
-    UPROPERTY(VisibleDefaultsOnly)
-	float MyInt5;
-
-    UPROPERTY(VisibleInstanceOnly)
-	float MyInt6;
-
-    UPROPERTY(BlueprintReadOnly)
-    bool bMyInt7;
-
-    UPROPERTY(BlueprintReadWrite)
-	bool bMyInt8;
-
-	UFUNCTION(BlueprintCallable)
-	void SetMyAwesomeProperty(int32 newValue);
-
-	UFUNCTION(BlueprintPure)
-	int32 GetMyAwesomeProperty() const;
-
-	UPROPERTY(BlueprintSetter = SetMyAwesomeProperty, BlueprintGetter = GetMyAwesomeProperty)
-	int32 AwesomeInt;
-
-	UFUNCTION(BlueprintCallable)
-	void FunctionA();
-
-	UFUNCTION(BlueprintPure)
-	int32 FunctionB();
-
-	// Static names for axis bindings
-	static const FName MoveForwardBinding;
-	static const FName MoveRightBinding;
-	static const FName FireForwardBinding;
-	static const FName FireRightBinding;
-
-private:
-
-	/* Flag to control firing  */
-	uint32 bCanFire : 1;
-
-	/** Handle for efficient management of ShotTimerExpired timer */
-	FTimerHandle TimerHandle_ShotTimerExpired;
-
-public:
-	/** Returns ShipMeshComponent subobject **/
-	FORCEINLINE class UStaticMeshComponent* GetShipMeshComponent() const { return ShipMeshComponent; }
-	/** Returns CameraComponent subobject **/
-	FORCEINLINE class UCameraComponent* GetCameraComponent() const { return CameraComponent; }
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 };
 
