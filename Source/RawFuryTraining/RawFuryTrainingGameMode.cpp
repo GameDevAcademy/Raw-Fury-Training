8dfd6c85d35b1d2945aab345a5e5c85590abac48
Alexandru Oprea <oalexprea@yahoo.com>
2021-02-22 20:00:29 +0200

    spawnning two players

--- Source/RawFuryTraining/RawFuryTrainingGameMode.cpp
+++ Source/RawFuryTraining/RawFuryTrainingGameMode.cpp
@@ -3,9 +3,48 @@
 #include "RawFuryTrainingGameMode.h"
 #include "RawFuryTrainingPawn.h"
 
+#include "RawFuryTraining.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerStart.h"
+
 ARawFuryTrainingGameMode::ARawFuryTrainingGameMode()
 {
 	// set default pawn class to our character class
-	DefaultPawnClass = ARawFuryTrainingPawn::StaticClass();
+	SpaceshipClass = ARawFuryTrainingPawn::StaticClass();
+}
+
+void ARawFuryTrainingGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+    TArray<AActor*> SpawnLocations;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), SpawnLocations);
+
+	for (int32 SpawnIndex = 0; SpawnIndex < SpawnLocations.Num(); SpawnIndex++)
+	{
+		const FTransform& SpawnTransform = SpawnLocations[SpawnIndex]->GetTransform();
+		SpawnPlayer(SpawnTransform, SpawnIndex);
+	}
+}
+
+void ARawFuryTrainingGameMode::SpawnPlayer(const FTransform& SpawnTransform, int32 PlayerIndex)
+{
+	if (SpaceshipClass == nullptr)
+	{
+		UE_LOG(LogRawFuryTraining, Error, TEXT("Missing Spaceship class on gamemode!"));
+		return;
+	}
+
+	// Create a controller if needed.
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), PlayerIndex);
+	if (!PlayerController)
+	{
+		PlayerController = UGameplayStatics::CreatePlayer(GetWorld(), PlayerIndex, true);
+	}
+
+	// Create actor and connect control.
+	ARawFuryTrainingPawn* SpaceShipActor = GetWorld()->SpawnActor<ARawFuryTrainingPawn>(SpaceshipClass, SpawnTransform);
+	PlayerController->Possess(SpaceShipActor);
 }
 
--- Source/RawFuryTraining/RawFuryTrainingGameMode.h
+++ Source/RawFuryTraining/RawFuryTrainingGameMode.h
@@ -6,6 +6,8 @@
 #include "GameFramework/GameModeBase.h"
 #include "RawFuryTrainingGameMode.generated.h"
 
+class ARawFuryTrainingPawn;
+
 UCLASS(MinimalAPI)
 class ARawFuryTrainingGameMode : public AGameModeBase
 {
@@ -13,6 +15,17 @@ class ARawFuryTrainingGameMode : public AGameModeBase
 
 public:
 	ARawFuryTrainingGameMode();
+
+protected:
+    /** The default spaceship class used by players. */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Classes)
+    TSubclassOf<ARawFuryTrainingPawn> SpaceshipClass;
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    void SpawnPlayer(const FTransform& SpawnTransform, int32 PlayerIndex);
 };
 
 
