d56b363d55bb415b9474425bd2cd438ebb01faaa
Alexandru Oprea <oalexprea@yahoo.com>
2021-02-22 17:49:53 +0200

    fixed hardcoded path

--- Source/RawFuryTraining/RawFuryTrainingProjectile.cpp
+++ Source/RawFuryTraining/RawFuryTrainingProjectile.cpp
@@ -9,12 +9,9 @@
 
 ARawFuryTrainingProjectile::ARawFuryTrainingProjectile() 
 {
-	// Static reference to the mesh to use for the projectile
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> ProjectileMeshAsset(TEXT("/Game/TwinStick/Meshes/TwinStickProjectile.TwinStickProjectile"));
-
+	
 	// Create mesh component for the projectile sphere
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileMesh0"));
-	ProjectileMesh->SetStaticMesh(ProjectileMeshAsset.Object);
 	ProjectileMesh->SetupAttachment(RootComponent);
 	ProjectileMesh->BodyInstance.SetCollisionProfileName("Projectile");
 	ProjectileMesh->OnComponentHit.AddDynamic(this, &ARawFuryTrainingProjectile::OnHit);		// set up a notification for when this component hits something
