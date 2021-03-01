24edea2a1ae7604992e0c49bd64bcd344b17d3ce
Alexandru Oprea <oalexprea@yahoo.com>
2021-03-01 23:59:45 +0200

    added pause button

---
+++ enc_temp_folder/4093fc122664d217de7ccad14d9d3cb8/RawFuryScoreWidget.cpp
@@ -0,0 +1,39 @@
+// Copyright Out-of-the-Box Plugins 2018-2020. All Rights Reserved.
+
+#include "RawFuryScoreWidget.h"
+
+#include "Widgets/Layout/SSpacer.h"
+
+void SRawFuryScoreWidget::Construct(const FArguments& InArgs)
+{
+    static const FTextBlockStyle ScoreTextStyle = FTextBlockStyle(FCoreStyle::Get().GetWidgetStyle<FTextBlockStyle>("NormalText"))
+        .SetFontSize(50);
+
+    ChildSlot
+        [
+            SNew(SHorizontalBox)
+            +SHorizontalBox::Slot()
+            [
+                SNew(SSpacer)
+            ]
+
+            +SHorizontalBox::Slot()
+            [
+                SAssignNew(TextWidget, STextBlock)
+                .Justification(ETextJustify::Center)
+                .TextStyle(&ScoreTextStyle)
+            ]
+
+            +SHorizontalBox::Slot()
+            [
+                SNew(SSpacer)
+            ]
+        ];
+
+    SetVisibility(EVisibility::HitTestInvisible);
+}
+
+void SRawFuryScoreWidget::SetWidgetText(FText Message)
+{
+    TextWidget->SetText(Message);
+}
