0523c4ab25f4ee6304a44a80330a4f0f725344a8
Alexandru Oprea <oalexprea@yahoo.com>
2021-04-10 19:42:30 +0300

    course finished

--- Source/RawFuryTraining/Private/RawFuryScoreWidget.cpp
+++ Source/RawFuryTraining/Private/RawFuryScoreWidget.cpp
@@ -3,41 +3,6 @@
 #include "RawFuryScoreWidget.h"
 
 #include "Widgets/Layout/SSpacer.h"
-#include <iostream>
-#include <functional>
-#include <vector>
-#include <algorithm>
-
-namespace
-{
-    int PrintA(int numbersToPrint, float valueToPrint)
-    {
-        for (int i = 0; i < numbersToPrint; i++)
-        {
-            std::cout << valueToPrint;
-        }
-
-        return numbersToPrint;
-    }
-
-    int PrintB(int numbersToPrint, float valueToPrint)
-    {
-        std::cout << "B";
-
-        return 0;
-    }
-
-    bool compare(int a, int b)
-    {
-        return a > b;
-    }
-
-    void DoStuff()
-    {
-        std::vector<int> myInt = { 1,2,3,4,6 };
-        std::sort(myInt.begin(), myInt.end(), compare);
-    }
-}
 
 void SRawFuryScoreWidget::Construct(const FArguments& InArgs)
 {
